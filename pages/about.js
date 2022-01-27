2eb0fb756b8ec7108a69b5b5973f81a35731745f
JoelM <joelmcmahon2@gmail.com>
2022-01-27 17:10:12 +0000

    initialise nextJS app

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h2>About</h2>
+      <p>Wassssuuuupp</p>
+    </div>
+  );
+};
+
+export default about;
